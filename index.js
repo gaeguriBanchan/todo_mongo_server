f50a6d4353cd87d82ddb588562ef95c230aa0942
gaeguriBanchan <rlawowls93@gmail.com>
2023-01-13 12:17:21 +0900

    최종빌드

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 // express 서버
-const express = require("express");
+const express = require('express');
 // 서버 경로 모듈
-const path = require("path");
+const path = require('path');
 // mongoose 모듈
-const mongoose = require("mongoose");
-const { Todo } = require("./model/TodoModel.js");
+const mongoose = require('mongoose');
+// const { Todo } = require("./model/TodoModel.js");
+
+var cors = require('cors');
 
 // 개발 인증관련
-const config = require("./config/key.js");
-const { response } = require("express");
+const config = require('./config/key.js');
+const { response } = require('express');
 
 // express 인스턴스 생성
 const app = express();
 // 포트번호
 const port = 5000;
+
+let corsOptions = {
+  origin: '*', // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+};
+
+app.use(cors(corsOptions));
 // 고정(Static)된 Path 경로를 설정한다.
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, './build')));
 // 요청이 들어오면 json 사용 및 url 인코딩 진행해줌
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Post 관련 Router연결
+app.use('/api/post', require('./router/Post.js'));
+// User 관련 Router연결
+app.use('/api/user', require('./router/User.js'));
 
 // 서버가 요청을 받아들이기 위해서 대기 중.
 app.listen(port, () => {
@@ -26,7 +40,7 @@ app.listen(port, () => {
   mongoose
     .connect(config.mongoURI)
     .then(() => {
-      console.log("DB 연결 성공");
+      console.log('DB 연결 성공');
       console.log(`Example app listening on port ${port}`);
     })
     .catch((err) => {
@@ -36,101 +50,103 @@ app.listen(port, () => {
 
 // 요청 : Request
 // 응답 : Response
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // 파일을 보여줌
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 //  주소가 없는 경우에 강제 URL 이동
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// 할일 등록
-app.post("/api/post/submit", (req, res) => {
-  // console.log(req.body);
-  let temp = req.body;
-  const todoPost = new Todo(temp);
-  todoPost
-    .save()
-    .then(() => {
-      // 데이터 저장 성공한 경우
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      // 데이터 저장 실패한 경우
-      console.log(err);
-      res.status(400).json({ success: false });
-    });
-});
+// // 할일 등록
+// router.post("/api/post/submit", (req, res) => {
+//   // console.log(req.body);
+//   let temp = req.body;
+//   const todoPost = new Todo(temp);
+//   todoPost
+//     .save()
+//     .then(() => {
+//       // 데이터 저장 성공한 경우
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       // 데이터 저장 실패한 경우
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
 
-// 목록 읽어오기
-app.post("/api/post/list", (req, res) => {
-  console.log("전체목록 호출");
-  Todo.find({})
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      res.status(200).json({ success: true, initTodo: doc });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({ success: false });
-    });
-});
-// 할일의 completed 를 업데이트
-app.post("/api/post/updatetoggle", (req, res) => {
-  let temp = {
-    completed: req.body.completed,
-  };
-  // mongoose 문서참조
-  Todo.updateOne({ id: req.body.id }, { $set: temp })
-    .exec()
-    .then(() => {
-      // console.log("completed 업데이트 완료");
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// 타이틀 업데이트
-app.post("/api/post/updatetitle", (req, res) => {
-  let temp = {
-    title: req.body.title,
-  };
-  Todo.updateOne({ id: req.body.id }, { $set: temp })
-    .exec()
-    .then(() => {
-      console.log("title 업데이트 완료");
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// 할일 삭제
-app.post("/api/post/delete", (req, res) => {
-  console.log(req.body);
-  Todo.deleteOne({ id: req.body.id })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ success: false });
-    });
-});
-// 전체 할일 삭제
-app.post("/api/post/deleteall", (req, res) => {
-  Todo.deleteMany()
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ success: false });
-    });
-});
+// // 목록 읽어오기
+// router.post("/api/post/list", (req, res) => {
+//   console.log("전체목록 호출");
+//   Todo.find({})
+//     .exec()
+//     .then((doc) => {
+//       console.log(doc);
+//       res.status(200).json({ success: true, initTodo: doc });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.status(400).json({ success: false });
+//     });
+// });
+// // 할일의 completed 를 업데이트
+// router.post("/api/post/updatetoggle", (req, res) => {
+//   let temp = {
+//     completed: req.body.completed,
+//   };
+//   // mongoose 문서참조
+//   Todo.updateOne({ id: req.body.id }, { $set: temp })
+//     .exec()
+//     .then(() => {
+//       // console.log("completed 업데이트 완료");
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+// // 타이틀 업데이트
+// router.post("/api/post/updatetitle", (req, res) => {
+//   let temp = {
+//     title: req.body.title,
+//   };
+//   Todo.updateOne({ id: req.body.id }, { $set: temp })
+//     .exec()
+//     .then(() => {
+//       console.log("title 업데이트 완료");
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+// // 할일 삭제
+// router.post("/api/post/delete", (req, res) => {
+//   console.log(req.body);
+//   Todo.deleteOne({ id: req.body.id })
+//     .exec()
+//     .then(() => {
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
+// // 전체 할일 삭제
+// router.post("/api/post/deleteall", (req, res) => {
+//   Todo.deleteMany()
+//     .exec()
+//     .then(() => {
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
+
+app.post('/api/user/register', (req, res) => {});
--- model/TodoModel.js
+++ model/TodoModel.js
@@ -1,11 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema(
   {
     id: Number,
     title: String,
     completed: Boolean,
+    uid: String,
+    // objextId를 통해서 다른 Model접근
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
-  { collection: "todos" }
+  { collection: 'todos' }
 );
-const Todo = mongoose.model("Todo", todoSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 module.exports = { Todo };
